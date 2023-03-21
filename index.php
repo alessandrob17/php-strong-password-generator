8283d1b6264d0b5bfc5348fe21174e9b7cd6e7e9
alessandrob17 <a.baldipardi@gmail.com>
2023-03-21 16:47:33 +0100

    FEAT get

--- index.php
+++ index.php
@@ -3,9 +3,30 @@
 <body>
 
 <form action="index.php" method="GET">
-Scegli lunghezza password: <input type="text" name="Password"><br>
+Scegli lunghezza password: <input type="text" name="password"><br>
 <input type="submit">
 </form>
 
+</body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<p>
+<?php
+
+echo $_GET [$password];
+
+?>
+</p>
+    
 </body>
 </html>
